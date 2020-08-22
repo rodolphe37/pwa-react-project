26fcefc58b866260f4678d0e59c41eed227e7f45
rodolphe37 <rodolphe.a@gmail.com>
2020-08-22 11:20:51 +0200

    fetch data from jsonplaceholder api, add users page to cache & display basic informations in table on screen

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap'
 
 const Users = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    let url = "https://jsonplaceholder.typicode.com/users"
+    fetch(url).then((response) => {
+      response.json().then((result) => {
+        console.warn(result)
+        setData(result)
+      })
+    })
+  }, [])
   return (
     <div>
-      <h1>Users Component</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.address.street}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
     </div>
   )
 }
