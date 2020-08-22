713cdff3cec69dd5c0597b606fb6b4ff979ceacb
rodolphe37 <rodolphe.a@gmail.com>
2020-08-22 09:31:15 +0200

    Instal rect-router-dom & add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="users">Users</Link></Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
+        <Switch>
+          <Route path='/about' component={About}></Route>
+          <Route path='/users' component={Users}></Route>
+          <Route path='/' component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
