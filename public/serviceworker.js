6965bcafdd25aca18468328d4a7513cb1c4d7f53
rodolphe37 <rodolphe.a@gmail.com>
2020-08-22 10:19:20 +0200

    add some codes in serviceworker file for offline mode

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,24 @@
-console.warn("sw file in public folder")
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        '/static/js/main.chunk.js',
+        '/static/js/0.chunk.js',
+        '/static/js/bundle.js',
+        '/index.html',
+        '/'
+      ])
+    })
+  )
+})
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      if (resp) {
+        return resp
+      }
+    })
+  )
+})
