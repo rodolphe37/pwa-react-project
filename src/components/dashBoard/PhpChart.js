523b6ebfeeb0394ce5b2bfff814fb6621d555035
rodolphe37 <rodolphe.a@gmail.com>
2020-08-26 20:37:08 +0200

    modification of java and ruby graphics, addition of more precision (a… …
    ddition of quarters 1, 2, 3 and 4 of each year)

--- src/components/dashBoard/PhpChart.js
+++ src/components/dashBoard/PhpChart.js
@@ -20,13 +20,24 @@ export class PhpChart extends Component {
           count.push(record.count);
           name.push(record.name)
         });
+
+        const [firstYear] = [parseInt(count[3]) + parseInt(count[31]) + parseInt(count[60])]
+        const [secondYear] = [parseInt(count[90]) + parseInt(count[130]) + parseInt(count[172]) + parseInt(count[226])]
+        const [thirdYear] = [parseInt(count[280]) + parseInt(count[341]) + parseInt(count[409]) + parseInt(count[480])]
+        const [fourthYear] = [parseInt(count[555]) + parseInt(count[645]) + parseInt(count[730]) + parseInt(count[820])]
+        const [fifthYear] = [parseInt(count[923]) + parseInt(count[1031]) + parseInt(count[1139]) + parseInt(count[1241])]
+        const [sixthYear] = [parseInt(count[1352]) + parseInt(count[1457]) + parseInt(count[1562]) + parseInt(count[1656])]
+        const [seventhYear] = [parseInt(count[1750]) + parseInt(count[1842]) + parseInt(count[1930]) + parseInt(count[2014])]
+        const [eighthYear] = [parseInt(count[2094]) + parseInt(count[2174]) + parseInt(count[2251]) + parseInt(count[2336])]
+        const [ninthYear] = [parseInt(count[2415]) + parseInt(count[2497])]
+
         this.setState({
           Data: {
             labels: [year[86], year[276], year[550], year[918], year[1348], year[1743], year[2087], year[2407], year[2574]],
             datasets: [
               {
                 label: name[3],
-                data: [count[3], count[90], count[280], count[555], count[923], count[1352], count[1750], count[2094], count[2415]],
+                data: [firstYear, secondYear, thirdYear, fourthYear, fifthYear, sixthYear, seventhYear, eighthYear, ninthYear],
                 backgroundColor: [
                   'rgba(255, 148, 120, 0.6)',
                   'rgba(255, 99, 132, 0.6)',
