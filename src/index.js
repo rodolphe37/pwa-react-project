dcee375034a0a9da8c1178fdc1b9f64eb1f9577c
rodolphe37 <rodolphe.a@gmail.com>
2020-08-30 21:03:14 +0200

    refactoring structure infiniteLoopLoader component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import swDev from './serviceworkerDev'
-import InfiniteLoopLoader from './components/InfiniteLoopLoader'
+import InfiniteLoopLoader from './components/infiniteLoopLoader/InfiniteLoopLoader'
 import "./i18n";
 require('dotenv').config();
 
