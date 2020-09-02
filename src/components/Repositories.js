705bfb77ff2b6eba957b655e084cd4ac978f8332
rodolphe37 <rodolphe.a@gmail.com>
2020-09-02 07:35:41 +0200

    some little modifs on repositories file

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -34,85 +34,87 @@ const Users = () => {
 
   return (
     <div>
-      {loading === false
-        ? (
-          <React.Fragment>
-            <div>
-              {
-                mode === 'offline' ? <div className=" alert alert-danger" role="alert">{t('alert')}</div> : null
-              }
-            </div>
-            <br />
-            <div><b>{t('reposList')}</b></div>
-            <br />
-            <Table striped bordered hover >
-              <thead>
-                <tr>
-                  <th className="hidden">Id</th>
-                  <th>{t('nameRepo')}</th>
-                  <th>{t('linkRepo')}</th>
-                  <th className="hidden">{t('createdrepos')}</th>
-                  <th className="hidden">{t('updatedRops')}</th>
-                  <th className="hidden">{t('languagesRepo')}</th>
-                </tr>
-              </thead>
-              <tbody>
+      {
+        loading === false
+          ? (
+            <React.Fragment>
+              <div>
                 {
-                  data.map((item) => (
-                    item.fork === false
+                  mode === 'offline' ? <div className=" alert alert-danger" role="alert">{t('alert')}</div> : null
+                }
+              </div>
+              <br />
+              <div><b>{t('reposList')}</b></div>
+              <br />
+              <Table striped bordered hover >
+                <thead>
+                  <tr>
+                    <th className="hidden">Id</th>
+                    <th>{t('nameRepo')}</th>
+                    <th>{t('linkRepo')}</th>
+                    <th className="hidden">{t('createdrepos')}</th>
+                    <th className="hidden">{t('updatedRops')}</th>
+                    <th className="hidden">{t('languagesRepo')}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    data.map((item) => (
+                      item.fork === false
+                        ?
+                        <tr key={item.id}>
+                          <td className="hidden">{idplus++}</td>
+                          <td>{item.name}</td>
+                          <td><a href={`${item.html_url}`} target="new">{t('clickOpen')}</a></td>
+                          <td className="hidden"><Moment locale="fr">{item.created_at}</Moment></td>
+                          <td className="hidden"><Moment locale="fr">{item.updated_at}</Moment></td>
+                          <td className="hidden">{item.language}</td>
+                        </tr>
+                        : null
+                    ))
+                  }
+                </tbody>
+              </Table>
+              <br />
+              <div><b>{t('forkedRepos')}</b></div>
+              <br />
+              <Table striped bordered hover >
+                <thead>
+                  <tr>
+                    <th className="hidden">Id</th>
+                    <th>{t('nameRepo')}</th>
+                    <th>{t('linkRepo')}</th>
+                    <th className="hidden">{t('createdrepos')}</th>
+                    <th className="hidden">{t('boolForked')}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item) => (
+                    item.fork === true
                       ?
                       <tr key={item.id}>
                         <td className="hidden">{idplus++}</td>
                         <td>{item.name}</td>
                         <td><a href={`${item.html_url}`} target="new">{t('clickOpen')}</a></td>
                         <td className="hidden"><Moment locale="fr">{item.created_at}</Moment></td>
-                        <td className="hidden"><Moment locale="fr">{item.updated_at}</Moment></td>
-                        <td className="hidden">{item.language}</td>
+                        <td className="hidden">{item.fork === true ? `${t('yesForked')}` : ''}</td>
+
                       </tr>
                       : null
-                  ))
-                }
-              </tbody>
-            </Table>
-            <br />
-            <div><b>{t('forkedRepos')}</b></div>
-            <br />
-            <Table striped bordered hover >
-              <thead>
-                <tr>
-                  <th className="hidden">Id</th>
-                  <th>{t('nameRepo')}</th>
-                  <th>{t('linkRepo')}</th>
-                  <th className="hidden">{t('createdrepos')}</th>
-                  <th className="hidden">{t('boolForked')}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item) => (
-                  item.fork === true
-                    ?
-                    <tr key={item.id}>
-                      <td className="hidden">{idplus++}</td>
-                      <td>{item.name}</td>
-                      <td><a href={`${item.html_url}`} target="new">{t('clickOpen')}</a></td>
-                      <td className="hidden"><Moment locale="fr">{item.created_at}</Moment></td>
-                      <td className="hidden">{item.fork === true ? `${t('yesForked')}` : ''}</td>
-
-                    </tr>
-                    : null
-                ))}
-              </tbody>
-            </Table>
-            <div className="padding-bottom-phone">
-              <br />
-              <br />
-              <br />
-              <br />
-            </div>
-          </React.Fragment>
-        ) : (
-          <InfiniteLoopLoader />
-        )}
+                  ))}
+                </tbody>
+              </Table>
+              <div className="padding-bottom-phone">
+                <br />
+                <br />
+                <br />
+                <br />
+              </div>
+            </React.Fragment>
+          ) : (
+            <InfiniteLoopLoader />
+          )
+      }
     </div>
   )
 }
